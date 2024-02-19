9246d3e8ddad62cad4acfd1aeacf49c9fe8ea73c
Alex Wise <alexwise@Alexs-MacBook-Pro.local>
2024-02-19 10:06:57 -0600

    make it better

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,19 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 # Seed the RottenPotatoes DB with some movies.
-# Load the Rails environment
-require "#{Rails.root}/config/environment.rb"
+# seeds.rb
 
-# Define more_movies array
 more_movies = [
   { title: 'My Neighbor Totoro', rating: 'G', release_date: '16-Apr-1988' },
   { title: 'Green Book', rating: 'PG-13', release_date: '16-Nov-2018' },
   { title: 'Parasite', rating: 'R', release_date: '30-May-2019' },
   { title: 'Nomadland', rating: 'R', release_date: '19-Feb-2021' },
-  { title: 'CODA', rating: 'PG-13', release_date: '13-Aug-2021' }
+  { title: 'CODA', rating: 'PG-13', release_date: '13-Aug-2021' },
+  { title: 'Inception', rating: 'PG-13', release_date: '16-Jul-2010' },
+  { title: 'The Shawshank Redemption', rating: 'R', release_date: '14-Oct-1994' },
+  { title: 'The Godfather', rating: 'R', release_date: '24-Mar-1972' }
 ]
 
-# Loop through more_movies and create Movie records
 more_movies.each do |movie|
-  Movie.create!(movie)
+  Movie.find_or_create_by!(movie)
 end
